98d12a0f8874c9bc929b01e52dae54500aee99bb
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2024-05-22 22:15:30 -0500

    recommit from previous

---
+++ carousel/src/photos.js
@@ -0,0 +1,20 @@
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
+
+const photos = [
+  {
+    src: image1,
+    caption: "Photo by Richard Pasquarella on Unsplash",
+  },
+  {
+    src: image2,
+    caption: "Photo by Pratik Patel on Unsplash",
+  },
+  {
+    src: image3,
+    caption: "Photo by Josh Post on Unsplash",
+  },
+];
+
+export default photos;
